853d5b650a9b02b63bc1c495a36f77bb48ba777a
curpish <ravenoftheredsky@gmail.com>
2025-05-03 15:00:29 -0400

    htmlnode.py

---
+++ src/htmlnode.py
@@ -0,0 +1,26 @@
+def class HTMLNode():
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        if not self.props:
+            return ""
+        props_str = ""
+        for key, value in self.props.items():
+            props_str += f' {key}="{value}"'
+        return props_str
+
+    def __repr__(self):
+        return (
+            f"HTMLNode(tag={self.tag}, "
+            f"value={self.value}, "
+            f"children={self.children}, "
+            f"props={self.props}), "
+        )
+

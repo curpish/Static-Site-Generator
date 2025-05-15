a676cadec49b37c3c75cddeb408a189b11789dd1
curpish <ravenoftheredsky@gmail.com>
2025-05-15 17:43:45 -0400

    passed test  gencontent

--- src/main.py
+++ src/main.py
@@ -1,8 +1,29 @@
-from textnode import *
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+from gencontent import generate_page
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
 
 def main():
-    test_node = TextNode('test text',TextType.BOLD_TEXT)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
 
 
-    print (test_node)
 main()
